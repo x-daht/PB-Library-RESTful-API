8b4a2f55eeb82122ec19b70dcb85581de268499a
David Hernandez <davidandreshernandeztriana@gmail.com>
2022-06-13 23:45:59 -0500

    feat (services - data (contracts - clients)): add repository manager contract and client

---
+++ Source/Library-RESTful-API/Data/Contracts/IRepositoryManager.cs
@@ -0,0 +1,25 @@
+﻿namespace Data.Contracts;
+
+/// <summary>
+/// Manager [contract] | [Repository - Declaration]
+/// </summary>
+public interface IRepositoryManager
+{
+    ICountryRepository<short> Country { get; }
+
+    IAuthorRepository<int> Author { get; }
+
+    ILiteraryGenreRepository<short> LiteraryGenre { get; }
+
+    ILiterarySubgenreRepository<short> LiterarySubgenre { get; }
+
+    IPublisherRepository<ushort> Publisher { get; }
+
+    ILanguageRepository<short> Language { get; }
+
+    IBookRepository<int> Book { get; }
+
+    IMagazineRepository<int> Magazine { get; }
+
+    IFormatRepository<short> Format { get; }
+}
